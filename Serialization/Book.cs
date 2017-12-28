72c4b82201857fe2a27b81d2ca1bd606dae4fa99
Aida7 <33822250+Aida7@users.noreply.github.com>
2017-12-28 21:15:39 +0600

    Добавьте файлы проекта.

---
+++ Serialization/Book.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Xml.Serialization;
+using System.IO;
+using Newtonsoft.Json;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace Serialization
+{
+
+    public class Book
+    { private int Year { get; set; }
+        private int Prise { get; set; }
+        private string Auhtor { get; set; }
+        private string Name { get; set; }
+
+        public  Book()
+        {
+            Year = 0;
+            Prise = 0;
+            Auhtor = " noname";
+            Name = "noname";
+        }
+    }      
+}
+
---
+++ Serialization/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Xml.Serialization;
+using System.IO;
+using Newtonsoft.Json;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace Serialization
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            List<Book> listPerson = new List<Book>();
+
+            XmlSerializer formater = new XmlSerializer(typeof(List<Book>));
+            
+            using (FileStream straem = new FileStream(@"c:\csv.bin", FileMode.OpenOrCreate))
+            {
+               
+                formater.Serialize(straem, listPerson);
+            }
+            using (FileStream straem = new FileStream(@"c:\csv.bin", FileMode.OpenOrCreate))
+            {
+                Book gettingOrder = formater.Deserialize(straem) as Book;
+            }
+            string data = JsonConvert.SerializeObject(listPerson);
+            var newOrder = JsonConvert.DeserializeObject<List<Book>>(data);
+
+        }
+    }
+}
